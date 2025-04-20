e4cb1cced62bc3f83d1caed6ed4f9c21b449a650
Polfirt <157729738+Polfirt@users.noreply.github.com>
2025-04-20 23:34:09 +0300

    asgi.py

--- PythonProject5/library_project/library_project/asgi.py
+++ PythonProject5/library_project/library_project/asgi.py
@@ -1,16 +1,6 @@
-"""
-ASGI config for library_project project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 
+# Аналогично WSGI, но для асинхронных серверов
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_project.settings')
-
 application = get_asgi_application()
